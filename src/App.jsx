f6ec7786721e8084f2a81d9ffb35066269414156
Gokul <122973682+Gblnn@users.noreply.github.com>
2023-12-15 04:18:54 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import HomeUK from "./pages/home-uk";
 import HomeMalaysia from "./pages/home-malaysia";
+import HomeOnline from "./pages/home-online";
 
 export default function App(){
   return(
@@ -11,6 +12,7 @@ export default function App(){
       <Route index path = "/" element = {<Home/>}/>
       <Route index path = "/home-uk" element = {<HomeUK/>}/>
       <Route index path = "/home-malaysia" element = {<HomeMalaysia/>}/>
+      <Route index path = "/home-online" element = {<HomeOnline/>}/>
     </Routes>
     </BrowserRouter>
     </>
---
+++ src/pages/home-online.jsx
@@ -0,0 +1,36 @@
+import Banner from "../components/banner";
+import Desc from "../components/desc";
+import EventBanner from "../components/eventbanner";
+import HeaderBar from "../components/headerbar";
+import LocationBar from "../components/locationbar";
+import TextCard from "../components/textcard";
+import '../styles/style.css'
+import '../styles/utils.css'
+
+
+export default function HomeOnline(){
+    return(
+        <>
+        <LocationBar online="location_active"/>
+        <HeaderBar/>
+        <Banner heading="Heriot-Watt Online courses" location="ONLINE" img="/Online-campus.jpg" desc="Be The One to earn while you learn with our career enhancing Business and STEM online degree programmes."/>
+        <EventBanner heading="Malaysia's First Green Campus" desc="The 'green campus' stands out with Malaysia's first living grass roof. This is a fantastic green area, part of Putrajaya Lake’s ‘green continuum’, and the building design is a conscious reference." color="color_pink"/>
+        <Desc heading="Why choose Heriot Watt?" description="Heriot-Watt University enjoys an established reputation for world-class teaching and practical, leading-edge research and sustainable educational practices modelled after real world applications."/>
+        <div className="feature_container">
+            <TextCard heading="Why choose us?" description = "We were one of the first overseas university to set up in Dubai International academic City, offering quality British education to people in the Gulf area"/>
+            <TextCard heading="Go Global" description = "As a global university, Heriot-Watt offers inter-campus transfers and the chance to experience our UK and Malaysia campuses."/>
+            <TextCard heading="Smart Construction" description = "The Centre of Excellence in Smart Construction fosters creative collaborations between research teams, industry and UAE government within the construction sector."/>
+            <TextCard heading="Edinburgh Business" description = "The Edinburgh Business School offers premium business management education at both, undergraduate and postgraduate level, with programs in Accountancy, Business Management, Marketing and Strategy."/>
+        </div>
+        <div class="empty"></div>
+        <Desc heading="Study at our Malaysia Campus" description="We offer a range of programmes at degree entry, undergraduate, postgraduate level and PhD courses in the best curriculum. Our instructors offer the best academic experience."/>
+        <div className="feature_container">
+            <TextCard heading="Global College" description = "Our one year Degree Entry (Foundation) Programmes are tailored to the Bachelor degrees that we teach, giving you a pathway into the first year of our degrees"/>
+            <TextCard heading="Undergraduate" description = "Our portfolio of undergraduate programmes covers a range of management, engineering, built environment and fashion subjects."/>
+            <TextCard heading="Postgraduate taught" description = "We offer an impressive range of postgraduate taught programmes."/>
+            <TextCard heading="PhD study" description = "Research training in the form of PhD programmes is an important part of our offering at the Dubai campus."/>
+        </div>
+        <div class="empty"></div>
+        </>
+    )
+}
